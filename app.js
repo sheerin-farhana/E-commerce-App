36f6664b893383f81f40844bb7209ee72d4a5dc2
sheerin-farhana <sheerinfarhana25@gmail.com>
2023-08-29 16:47:06 +0530

    user and product relationship designed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
-const User = require('./models/user');
+const User = require('./models/user1');
 
 const app = express();
 app.use(cors());
@@ -20,14 +20,39 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+})
+
+
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(userRoutes);
 app.use(errorController.get404);
 
+Product.belongsTo(User,{constraints: true , onDelete : 'CASCADE'});
+User.hasMany(Product);
+
 sequelize.sync()
 .then((result) => {
+    return User.findByPk(1);
     console.log(result);
+    
+})
+.then(user => {
+    if(!user) {
+       return User.create({name:'Max' , email:"test@gmail.com"})
+    }
+    return Promise.resolve(user);
+})
+.then(user => {
+    // console.log(user);
     app.listen(3000);
 })
 .catch(err => console.log(err));
